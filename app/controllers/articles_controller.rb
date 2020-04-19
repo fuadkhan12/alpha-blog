8a757107eb32d8933fdb0875d8610620eb0323ed
Fuad Khan <khanfuad17@gmail.com>
2020-04-19 22:10:59 +0500

    Add edit and update actions for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,20 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if(@article.update(set_permitted_params))
+      redirect_to @article
+    else
+      render :edit
+    end
+
+  end
+
   private
     def set_permitted_params
       params.require(:article).permit(:title, :description)
