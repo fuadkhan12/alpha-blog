3d8eaac0ee032ace778fc75f79c75ffeeaa3b3a3
Fuad Khan <khanfuad17@gmail.com>
2020-04-19 15:17:03 +0500

    Created new article and handle validation error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,26 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(set_permitted_params)
+    if(@article.save)
+      flash[:notice] = "Article was created successfully!"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
+
+  private
+    def set_permitted_params
+      params.require(:article).permit(:title, :description)
+    end
 end
